41061b1874085fed7207cd8c52b96d65dba3eb66
oomae2469 <takalock@icloud.com>
2020-07-24 12:02:02 +0900

    create-newpost

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,4 +5,21 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    if Tweet.create(tweet_params)
+      redirect_to :tweets
+    else
+      render :new
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
 end
